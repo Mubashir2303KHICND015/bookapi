152abd23d75b2a3f7e7497ab715c053d1ff0652f
Syed_Mubashir_Alam <mubashiralam02@gmail.com>
2023-04-07 09:23:19 +0500

    Successfully failed test 4

--- src/test/java/com/glc/BookApiTest.java
+++ src/test/java/com/glc/BookApiTest.java
@@ -38,12 +38,27 @@ public class BookApiTest {
         //Setup
         ReadingList list = new ReadingList();
         Book book = new Book("Martian","Davidoff",362,2004);
-        Book book2 = new Book("Martian","Davidoff",362,2004);
+        Book book2 = new Book("Rich dad","markeloff",365,2024);
         list.addBook(book, "Sep 1", 5);
-        list.addBook(book2, "Sep 1", 5);
+        list.addBook(book2, "Sep 5", 1);
         //Execute
         int result = list.numberRead();
         //Assert
         assertEquals(2, result);
+    }
+
+    @Test
+    public void removeBookTest (){
+        //Setup
+        ReadingList list = new ReadingList();
+        Book book = new Book("Martian","Davidoff",362,2004);
+        Book book2 = new Book("Atomic Habbits","windi",305,2001);
+        list.addBook(book, "Sep 1", 5);
+        list.addBook(book2, "Sep 5", 4);
+        list.removeBook("Martian");
+        //Execute
+        int result = list.numberRead();
+        //Assert
+        assertEquals(1, result);
     }
 }
